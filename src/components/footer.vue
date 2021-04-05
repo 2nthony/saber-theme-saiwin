69b47c404ac8f9dd8b550ef09caea11122399076
evillt <ijoec123@gmail.com>
2021-04-06 00:16:23 +0800

    feat: add footnote support

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,14 @@
 <template>
   <footer class="text-center mt-12 text-sm my-4">
-    <div>
+    <p>
       2018 ~ {{ new Date().getFullYear() }}
       {{ $siteConfig.author }}
-    </div>
+    </p>
+
+    <p
+      class="mt-2"
+      v-if="$themeConfig.footnote"
+      v-html="$themeConfig.footnote"
+    ></p>
   </footer>
 </template>
